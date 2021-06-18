f44b8a9ac9ca9e2a065328bd9221532181e728a4
Mitakeey <mitakeeykiage@gmail.com>
2021-06-18 13:22:03 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Prints decimal digits
+ *
+ *Description: Prints decimal digits
+ *Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
