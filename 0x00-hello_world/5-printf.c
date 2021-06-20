3fcd9ff309b07fc4bbe55391a9ae3a4dfe2791f3
Mitakeey <mitakeeykiage@gmail.com>
2021-06-20 10:26:00 -0700

    printf with a zero return

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*Basic printf statements with return 0
+* main - main to print text out void
+* Return: 0
 */
-
-int main()
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art/,");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
- 
+
 }
