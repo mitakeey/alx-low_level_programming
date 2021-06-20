462ab666e997a07fd8124c0cf2ba0e257166f5d1
Mitakeey <mitakeeykiage@gmail.com>
2021-06-20 10:08:59 -0700

    using puts and escape characters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+/**
+ * main - main to print text out void
+ * Return: 0
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
