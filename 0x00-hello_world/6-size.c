9c18156fcdbe05e4a5475a12fb00d483da9dca22
Mitakeey <mitakeeykiage@gmail.com>
2021-06-20 12:01:58 -0700

    printing out size of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ long long loongType;
 float floatType;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of a int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long: %zu byte(s)\n", sizeof(longType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(loongType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
