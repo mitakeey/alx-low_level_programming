125901498aa62c79f76a210839e1ce9716a7dcaa
Mitakeey <mitakeeykiage@gmail.com>
2021-06-23 02:42:10 -0700

    2 challenge

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"holberton.h"
+/**
+* main - check the code for ALX School students.
+*
+* Return: 1 on success
+*/
+void print_alphabet(void)
+{
+char alf;
+
+for (alf = 'a'; alf <= 'z'; alf++)
+{
+_putchar(alf);
+}
+
+_putchar('\n');
+}
