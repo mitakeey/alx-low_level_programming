f76fac38a0249cbdab608d30ca0b7a5e87e8d32d
Mitakeey <mitakeeykiage@gmail.com>
2021-06-23 05:05:36 -0700

    10 number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: an integer arg
+ * @b: an integer arg
+ * Return: Void.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
