d37d456c7690fd20f4ebbf23ab593caf8828b415
Mitakeey <mitakeeykiage@gmail.com>
2021-06-24 09:24:40 -0700

    upper and lower case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
+
 /**
- * _isupper(int c)- function that checks for uppercase character
- *@c: ascii value of char
- * Return: 1 when lowercase , 0 when not.
- */
+* _isupper - checks uppercase letters
+* @c: takes character in ASCII
+*
+* Return: 0 or 1
+*/
+
 int _isupper(int c)
 {
-if (c > 64 && c < 91)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
