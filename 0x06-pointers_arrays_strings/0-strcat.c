bc4d587327a0cec54bc52b0923897e7554b63228
Mitakeey <mitakeeykiage@gmail.com>
2021-07-01 11:36:46 -0700

    changes strtcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,24 @@
  *
  * Return: a pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
 {
-int i, j;
-for (i = 0; dest[i] != '\0'; i++)
-{
-  for (j = 0; scr[j] != '\0'; j++, i++)
-{
-dest[i + j] = scr[j];
-}
-dest[i] = '\0';
-return (dest);
-}
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
