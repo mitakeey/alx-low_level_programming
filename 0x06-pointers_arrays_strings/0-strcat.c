4dd02f279be517a72f51c0f2390f8cccaec656cf
Mitakeey <mitakeeykiage@gmail.com>
2021-07-01 11:35:16 -0700

    changes strtcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,7 @@ char *_strcat(char *dest, char *src)
 int i, j;
 for (i = 0; dest[i] != '\0'; i++)
 {
-for (j = 0; scr[j] != '\0'; j++)
+  for (j = 0; scr[j] != '\0'; j++, i++)
 {
 dest[i + j] = scr[j];
 }
