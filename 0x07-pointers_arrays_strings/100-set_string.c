308267a6c245d023fb3c25dea311d5b523499289
Mitakeey <mitakeeykiage@gmail.com>
2021-07-05 13:18:45 -0700

    last advance

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - value of pointer to char
+ * @s: source 
+ * @to: target
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
