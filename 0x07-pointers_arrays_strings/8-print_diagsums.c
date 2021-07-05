881f21db0d601dfdf06517874286d23e811b2442
Mitakeey <mitakeeykiage@gmail.com>
2021-07-05 13:15:07 -0700

    last

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,22 +6,16 @@
  * @size: size of array (square)
  */
 
-
 void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
-    
-    for (i = 0; i < size; i++)
-    {
-        sum1 += a[i];
-        sum2 += a[size - i];
-        a += size;
-    }
-    
 
-
-}
-int main()
-{
-
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+			sum1 += *(a + i);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			sum2 += *(a + i);
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
