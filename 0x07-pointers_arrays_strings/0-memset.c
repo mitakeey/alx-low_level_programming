54f2f3ecae5087b7648a2ad78f4e3517a6535af9
Mitakeey <mitakeeykiage@gmail.com>
2021-07-05 12:21:04 -0700

    corrected memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,10 +8,15 @@
  *
  * Return: Nothing.
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int i);
 {
+unsigned int i;
+
 for (i = 0; i < n; i++)
+
 {
+*s[i] = b;
 
 }
+return (s);
 }
