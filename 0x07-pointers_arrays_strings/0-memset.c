a8fe956765ae0da6927cab1ce43bc3e243ece8a2
Mitakeey <mitakeeykiage@gmail.com>
2021-07-05 12:08:12 -0700

    first trial

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <holberton.h>
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @s: the pointer to be filled with s
+ * @b: the address of memory to print
+ * @n: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+for (i = 0; i < n; i++)
+{
+
+}
+}
