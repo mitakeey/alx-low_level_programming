748f191a03a423b712ad025ab890ce762e073e3b
Mitakeey <mitakeeykiage@gmail.com>
2021-07-14 00:37:02 -0700

    number 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all the arguments
+ * @argc: counts all arguments  into main
+ * @argv: stores all arguments  into main
+ *
+ * Return: an interger 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
+© 2021 GitHub, Inc.
